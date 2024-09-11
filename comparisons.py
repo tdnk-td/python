ef53dd5151cc4fd98b0b9ce53210cf474ec039ed
tdnk-td <tdnknight@gmail.com>
2024-09-10 20:31:07 -0400

    updated comparison.py
    completed all work within this chapter
    added additional comments

--- comparisons.py
+++ comparisons.py
@@ -54,6 +54,7 @@ def main():
 
 
 main()
+format()
 
 # 7.5 - If Practice
 def check_swords_for_army(number_of_swords, number_of_soldiers):
@@ -93,3 +94,53 @@ def does_attack_hit(attack_roll, armor_class):
             return True
         else:
             return False
+        
+# 7.10 - Boolean Quiz
+question(7.10, "Boolean Quiz")
+
+is_tall = True
+is_fat = True
+is_skinny = False
+is_short = False
+result = (is_tall and is_fat) or (is_skinny and is_short)
+# (is_tall and is_fat) = True
+# (is_skinny and is_short) = False
+# Since or has one true, therefore True [1 or 0] = 1
+print("Q: Boolean Quiz")
+print(f"A: {result} ")
+
+# 7.11 - Should Serve Drinks
+def should_serve_customer(customer_age, on_break, time):
+    if (customer_age >= 21 and on_break == False and time >= 5 and time <= 10): # if 5 <= time <= 10:
+        return True
+    else:
+        return False
+    
+# 7.97 - [C1] Mount Rental
+def check_mount_rental(time_used, time_purchased):
+    if (time_used >= time_purchased):
+        return "overtime charged"
+    else:
+        return "no charges yet"
+    
+# 7.98 - [C2] Combat Advantage
+def combat_evaluation(player_power, enemy_defense):
+    advantage, disadvantage, evenly_matched = False, False, False
+
+    if (player_power > enemy_defense):
+        advantage = True
+    elif (player_power < enemy_defense):
+        disadvantage = True
+    else: # elif (player_power == enemy_defense):
+        evenly_matched = True
+
+    return advantage, disadvantage, evenly_matched
+
+# 7.99 - [C3] Race
+def has_enough_energy(energy_available, distance_one_way, meters_per_energy):
+    total_dist = distance_one_way * 2
+    energy_needed = energy_available * meters_per_energy
+    if (energy_needed >= total_dist):
+        return True
+    else:
+        return False
--- comparisons/main_test12.py
+++ comparisons/main_test12.py
@@ -5,6 +5,7 @@ import os
 # Add the parent directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+
 from comparisons import *
 
 run_cases = [
